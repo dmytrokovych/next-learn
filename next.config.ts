224344913bfa5719dfc730df9b848d312c178640
Dima Konahovych <dimakonahovych@gmail.com>
2025-07-23 14:59:59 +0000

    db init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "local-origin.dev",
+    "*.local-origin.dev",
+    "https://5jdfg3-3000.csb.app",
+  ],
 };
 
 export default nextConfig;
